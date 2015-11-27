73320e83fcff2144130228072b00e920f692a48d
Engin <denganjun1234@qq.com>
2015-11-27 16:50:22 +0800

    add codemirror mode/ app.js

---
+++ src/js/app/app.js
@@ -0,0 +1,35 @@
+new function(window, document, CodeMirror, Clipboard) {
+
+
+    var sl = function(selector) {
+        return document.getElementById(selector)
+    }
+
+
+    var editor = CodeMirror.fromTextArea(sl("code-panel"), {
+        // value: value,
+        lineNumbers: true,
+        mode: "text/html",
+        keyMap: "sublime",
+        // autoCloseBrackets: true,
+        // matchBrackets: true,
+        // showCursorWhenSelecting: true,
+        // tabSize: 2,
+        theme: "monokai"
+    });
+
+
+    function doHighLight() {
+        CodeMirror.runMode(
+            editor.getValue(),
+            "text/html",
+            sl("output-panel"))
+    }
+    editor.on('change', doHighLight)
+    doHighLight()
+
+
+    new Clipboard('#btn-copy')
+
+
+}(window, document, CodeMirror, Clipboard)
