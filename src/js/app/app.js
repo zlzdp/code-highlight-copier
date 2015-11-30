9be1587166f514bf2d9a48a3c1e6ba162029524b
Engin <denganjun1234@qq.com>
2015-11-30 20:49:03 +0800

    css/ hotkeys /add more language support

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -1,6 +1,7 @@
 new function(window, document, CodeMirror, Clipboard) {
 
     var lang = 'text/html'
+    var langSelect = $('select[data-provide="select2"]')
     var editor = CodeMirror.fromTextArea($("#code-panel")[0], {
         // value: value,
         lineNumbers: true,
@@ -16,22 +17,47 @@ new function(window, document, CodeMirror, Clipboard) {
             lang,
             $("#output-panel")[0])
     }
+
     editor.on('change', doHighLight)
     doHighLight()
 
 
     new Clipboard('#btn-copy')
 
-    $('select[data-provide="select2"]')
-        .change(function() {
-            lang = $(this).val()
-            doHighLight()
-        })
-        .select2()
+    langSelect.change(function() {
+        lang = $(this).val()
+        editor.setOption("mode", lang)
+        doHighLight()
 
-    // 可能是 代码编辑器也监听了按钮，导致了监听键盘事件不正常执行
-    $('body,#code-panel').bind('keydown', 'Alt+l', function (e) {
-        // console.log(1)
+        setTimeout(function() {
+            editor.focus()
+            editor.execCommand('goDocEnd')
+        }, 15)
+
     })
+    langSelect.select2({
+        width: '7em'
+    })
+
+    // 可能是代码编辑器也监听了按钮，导致监听键盘事件不正常执行
+    // document 与 editor 分开监听事件
+    $(document).bind('keyup', 'Alt+l', function(e) {
+        // e.isDefaultPrevented()
+        langSelect.select2('open')
+    })
+    editor.on('keyup', function(codemirror, e) {
+        if (e.altKey && e.keyCode === 76) {
+            langSelect.select2('open')
+        }
+    })
+
+
+    ! function main() {
+        setTimeout(function() {
+            editor.focus()
+            editor.execCommand('goDocEnd')
+
+        }, 30)
+    }()
 
 }(window, document, CodeMirror, Clipboard)
